73ccf0ceda1810b124ec92c0eaa2ffd04b8cac78
Subin Lee <chandar.sblee@gmail.com>
2025-03-23 16:24:31 +0900

    Update send_message_request.py

---
+++ solapi/model/request/__init__.py
@@ -0,0 +1,2 @@
+# NOTE: Python SDK가 업데이트 될 때마다 Version도 갱신해야 함!
+VERSION = "python/5.0.0"
--- solapi/model/request/send_message_request.py
+++ solapi/model/request/send_message_request.py
@@ -2,10 +2,11 @@ import platform
 from datetime import datetime
 from typing import Optional, Union
 
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 
 from solapi.model.message import Message
+from solapi.model.request import VERSION
 
 
 class SendRequestConfig(BaseModel):
@@ -14,24 +15,19 @@ class SendRequestConfig(BaseModel):
     show_message_list: bool = False
     scheduled_date: Optional[Union[str, datetime]] = None
 
-    model_config = ConfigDict(
-        alias_generator=to_camel, extra="ignore", populate_by_name=True
-    )
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
 
 
 class SendMessageRequest(BaseModel):
     messages: list[Message]
-    scheduled_date: Optional[str] = Field(
-        default=None, serialization_alias="scheduledDate"
-    )
-    show_message_list: Optional[bool] = Field(
-        default=False, serialization_alias="showMessageList"
-    )
-    allow_duplicates: bool = Field(default=False, serialization_alias="allowDuplicates")
-    app_id: Optional[str] = Field(default=None, serialization_alias="appId")
+    scheduled_date: Optional[str] = None
+    show_message_list: Optional[bool] = None
+    allow_duplicates: bool = False
+    app_id: Optional[str] = None
 
-    # NOTE: Python SDK 버전 업데이트 할 때마다 변경해줘야 함
     agent: dict[str, str] = {
-        "sdkVersion": "python/5.0.0",
+        "sdkVersion": VERSION,
         "osPlatform": f"{platform.platform()} | {platform.python_version()}",
     }
+
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
